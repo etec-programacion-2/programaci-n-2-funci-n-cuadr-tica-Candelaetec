af4dd6b1942356401840dda413c4bf7b20467de9
{Candelaetec} <c.delacruz@alumno.etec.um.edu.ar>
2025-05-09 09:48:23 -0300

    feat: Mejoras en presentación y formato

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -10,13 +10,13 @@ const val TermB: Double = 12.00
 const val TermC: Double = 3.00
 
 fun main() {
-    
+    // representa si el termino A de la función cuadrática es 0 o no
     if (TermA == 0.0) {
         println("El término A es 0.")
     } else {
         println("El término A no es 0.")
     }
-
+    // Asigna valores del -5 al 5 a la variable x con paso de una unidad
     for (i in -5..5) {
         val x = i.toDouble()
         println("x = $x")
@@ -29,17 +29,23 @@ fun main() {
         println("Las raíces de la ecuación son: x1 = $root1, x2 = $root2")
     }
 }
-
+//calcularF es la función cuadrática
+//calcularF recibe los términos A, B y C de la función cuadrática y el valor de x. Pueden ser decimales
 fun calcularF(TermA: Double, TermB: Double, TermC: Double, x: Double): Double {
+    //calcularF devuelve el resultado de la función cuadrática
     return TermA * x * x + TermB * x + TermC
 }
 
 fun raiz(TermA: Double, TermB: Double, TermC: Double): Pair<Double?, Double?> {
+    //Calcular la discriminante
+    //Si la discriminante es menor que 0, no hay raíces reales
     val discriminante = TermB * TermB - 4 * TermA * TermC
     if (discriminante < 0) {
         println("No hay raíces reales.")
         return Pair(null, null)
     }
+    //sqrt sirve para calcular la raíz cuadrada
+    //RoundingMode.HALF_UP redondea el resultado a 2 decimalesssss
     val root1 = (-TermB + Math.sqrt(discriminante)) / (2 * TermA)
     val root1redondeado = root1.toBigDecimal().setScale(2, RoundingMode.HALF_UP).toDouble()
     val root2 = (-TermB - Math.sqrt(discriminante)) / (2 * TermA)
